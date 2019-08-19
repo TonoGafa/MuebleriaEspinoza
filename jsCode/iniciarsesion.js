820a29c2ef0b08a690b92425eb15dffec92865fd
Luis Antonio García Francisco <lgarcia@GRUPOHEMAC.LOCAL>
2019-08-19 18:00:05 -0500

    x
    x

--- jsCode/iniciarsesion.js
+++ jsCode/iniciarsesion.js
@@ -1,27 +1,42 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyDZQPI_DkvJYAZP6ySwU4x0v34RoUXUyu0",
-    authDomain: "prestacasa-1758e.firebaseapp.com",
-    databaseURL: "https://prestacasa-1758e.firebaseio.com",
-    projectId: "prestacasa-1758e",
-    storageBucket: "prestacasa-1758e.appspot.com",
-    messagingSenderId: "315948810598"
-  });
-  
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
+  apiKey: "AIzaSyDqfihS3c6xRg8GPG3LwedqxVbaWv80MVM",
+  authDomain: "prestacasa-ad0ec.firebaseapp.com",
+  databaseURL: "https://prestacasa-ad0ec.firebaseio.com",
+  projectId: "prestacasa-ad0ec",
+  storageBucket: "prestacasa-ad0ec.appspot.com",
+  messagingSenderId: "846656829348"
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
 
   function validarInicio(){
     var emailSesion = document.getElementById('emailSesion').value;
     var passwordSesion = document.getElementById('passwordSesion').value;
 
-    db.collection("Cobradores").onSnapshot((querySnapshot) =>{
-      querySnapshot.forEach((doc)=>{
+    try{
+        var existeCobrador = 0
 
-        if ((doc.data().Correo)==emailSesion){
-          alert("Email encontrado")
-        }
-
+        db.collection("Cobradoreee").get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            console.log(doc.Correo, " => ", doc.data());
+            if ((doc.data().Correo)==emailSesion ){
+              existeCobrador=existeCobrador+1;
+            }else
+            {
+              existeCobrador=2
+            }
+    
+          });
+          if  (existeCobrador>=1){
+            alert("Existe Cobrador")
+          }else{
+            alert("No existe cobrador")
+          }
       });
-  });
+  
+    }catch{
+      alert("Error al iniciar sesion")
+    }
 
   }
